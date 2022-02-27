acc9059006fa30813b68aac9d3e6cbc2070a50c0
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2022-02-27 16:52:04 +0600

    add

--- src/components/pages/AirTickets/AirTickets.js
+++ src/components/pages/AirTickets/AirTickets.js
@@ -3,7 +3,7 @@ import { Button, Container } from 'react-bootstrap';
 import AirTicketsBanner from '../AirTicketsBanner/AirTicketsBanner';
 import airTickets1 from '../../Images/air/Air Ticket Banner Image.jpg'
 import airTickets2 from '../../Images/air/International.jpg'
-import airTickets3 from '../../Images/air/Indian Domestic.jpg'
+// import airTickets3 from '../../Images/air/Indian Domestic.jpg'
 import airTickets4 from '../../Images/air/Domestic Airbus.jpg'
 import airTickets5 from '../../Images/air/Special Air.jpg'
 
@@ -20,7 +20,7 @@ const AirTickets = () => {
     </div>
 
     <h2 className='ariHeader_text my-3'>AIR TICKETS</h2>
- <Container className='air_container'>     
+        <Container className='air_container'>     
          
            <div className="box">
         <div className="imgBox">
@@ -47,7 +47,7 @@ const AirTickets = () => {
         </div>
     </div>
 
-    <div className="box">
+     {/*<div className="box">
         <div className="imgBox">
             <img src={ airTickets3} alt="" />
         </div>
@@ -55,11 +55,11 @@ const AirTickets = () => {
         <div className="content">
 
             <Button className='air_button'>DOMESTIC</Button>
-             {/*<h2>Aakash Agrawal
-            <br /> <span>Chartered Accountant C.A</span> </h2> */}
+            <h2>Aakash Agrawal
+            <br /> <span>Chartered Accountant C.A</span> </h2> 
            
         </div>
-    </div>
+    </div>*/}
 
 
        <div className="box">
@@ -75,7 +75,7 @@ const AirTickets = () => {
         </div>
     </div>
 
-       <div className="box">
+       <div className="box air_box4">
         <div className="imgBox">
             <img src={ airTickets5} alt="" />
         </div>
@@ -87,20 +87,6 @@ const AirTickets = () => {
            
         </div>
     </div>
-
-       {/*  <div className="box">
-        <div className="imgBox">
-            <img src="https://image.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg" alt="" />
-        </div>
-        
-        <div className="content">
-            <Button>Domestic</Button>
-           <h2>Aakash Agrawal</h2> <br /> <span>Chartered Accountant C.A</span> 
-           
-        </div>
-    </div>*/}
-    
-
     </Container>
 </div>
     );
